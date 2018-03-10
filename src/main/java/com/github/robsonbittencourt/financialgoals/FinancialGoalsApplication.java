d709a98eb452475f8bd16e053d5c23094579166c
robsonbittencourt <robson.luizv@gmail.com>
2018-03-10 13:09:37 -0300

    Initial commit

---
+++ src/main/java/com/github/robsonbittencourt/financialgoals/FinancialGoalsApplication.java
@@ -0,0 +1,12 @@
+package com.github.robsonbittencourt.financialgoals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialGoalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialGoalsApplication.class, args);
+	}
+}
